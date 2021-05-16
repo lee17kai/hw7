696b7d86a74b85052e97450e4204630a32f3b257
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-05-16 13:09:57 -0500

    0109

--- netlify/functions/courses.js
+++ netlify/functions/courses.js
@@ -58,28 +58,36 @@ exports.handler = async function(event) {
   // create an object with the course data to hold the return value from our lambda
   let returnValue = {
     courseNumber: courseData.courseNumber,
-    name: courseData.name
+    name: courseData.name,
+    sections: [],
+    courseReviewSummary: []
   }
-
-  // set a new Array as part of the return value
-  returnValue.sections = []
 
   // ask Firebase for the sections corresponding to the Document ID of the course, wait for the response
   let sectionsQuery = await db.collection('sections').where(`courseId`, `==`, courseId).get()
 
-  // get the documents from the query
+  // get the documents from the query .docs turns courseQuery into array
   let sections = sectionsQuery.docs
 
+  //Create a counter to track the total number of reviews per course
+  let countCourseReviews = 0
+  //Create a counter to track the sum of ratings per course
+  let sumCourseRatings = 0
+
   // loop through the documents
-  for (let i=0; i < sections.length; i++) {
+  for (let sectionIndex=0; sectionIndex < sections.length; sectionIndex++) {
     // get the document ID of the section
-    let sectionId = sections[i].id
+    let sectionId = sections[sectionIndex].id
 
     // get the data from the section
-    let sectionData = sections[i].data()
+    let sectionData = sections[sectionIndex].data()
     
     // create an Object to be added to the return value of our lambda
-    let sectionObject = {}
+    let sectionObject = {
+      lecturerName: "",
+      sectionReviews: [],
+      sectionReviewSummary: []
+    }
 
     // ask Firebase for the lecturer with the ID provided by the section; hint: read "Retrieve One Document (when you know the Document ID)" in the reference
     let lecturerQuery = await db.collection('lecturers').doc(sectionData.lecturerId).get()
@@ -94,7 +102,58 @@ exports.handler = async function(event) {
     returnValue.sections.push(sectionObject)
 
     // 🔥 your code for the reviews/ratings goes here
+
+    // ask Firebase for the reviews corresponding to the section ID of the course, wait for the response
+    let reviewsQuery = await db.collection('reviews').where(`sectionId`, `==`, sectionId).get()
+
+    // get the documents from the query .docs turns reviews Query into array
+    let reviews = reviewsQuery.docs
+
+    //Create counters to track total number of reviews per section and sum of ratings per section
+    let countSectionReviews = 0
+    let sumSectionRatings = 0
+
+    //loop through the reviews
+    for(reviewIndex = 0; reviewIndex < reviews.length; reviewIndex++) {
+      //get the document id of each review
+      let reviewId = reviews[reviewIndex].id
+
+      // get the data from each review
+      let reviewData = reviews[reviewIndex].data()
+
+      //create an object to be added to the return value of our lambda
+      let reviewObject = {}
+
+      // // //add the body to the review object
+      reviewObject.body = reviewData.body
+
+      //add the rating to the review object
+      reviewObject.rating = reviewData.rating
+
+      // add to counters
+      countSectionReviews = countSectionReviews + 1
+      sumSectionRatings = sumSectionRatings + Number(reviewObject.rating)
+      countCourseReviews = countCourseReviews + 1
+      sumCourseRatings = sumCourseRatings + Number(reviewObject.rating)
+
+      sectionObject.sectionReviews.push(reviewObject)
+    }
+    // create a section review summary object
+    let sectionReviewSummaryObject = {
+      numberOfSectionReviews: countSectionReviews,
+      averageSectionReviewRating: sumSectionRatings/countSectionReviews
+    }
+    //add the section review summary object to the section object
+    sectionObject.sectionReviewSummary.push(sectionReviewSummaryObject)
   }
+
+  // create a course review summary object
+  let courseReviewSummaryObject = {
+    numberOfCourseReviews: countCourseReviews,
+    averageCourseReviewRating: sumCourseRatings/countCourseReviews
+  }
+  //add the course review summary object to the return value
+  returnValue.courseReviewSummary.push(courseReviewSummaryObject)
 
   // return the standard response
   return {
